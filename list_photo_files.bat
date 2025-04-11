a395a459402a3156de28251e4788a45136884a64
Sujay Sreedhar <sujaysreedhar@gmail.com>
2025-04-11 21:29:46 +0530

    added disc for indian pages

--- list_photo_files.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Saving file list with folder structure to logs.txt...
-echo ------------------------------------------
-dir photos /b /s > logs.txt
-echo Done. Check logs.txt for results.
-pause
